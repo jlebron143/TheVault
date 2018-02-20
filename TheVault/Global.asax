177aa7800b33e68ad1b2bfa1515d5e03dc8c9459
jlebron143 <jlebron143@gmail.com>
2018-02-20 09:39:25 -0600

    Add project files.

---
+++ TheVault/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheVault.MvcApplication" Language="C#" %>
---
+++ TheVault/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheVault.Startup))]
+namespace TheVault
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
