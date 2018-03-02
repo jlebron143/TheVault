18bd122de2464f63f9295f75378662358a2e3ff8
jlebron143 <jlebron143@gmail.com>
2018-03-02 13:24:01 -0600

    added cart,order,orderdetailsw/shoppingcart,validrulesw/authuseer

--- TheVault/Models/TheVaultEntities.cs
+++ TheVault/Models/TheVaultEntities.cs
@@ -11,5 +11,9 @@ namespace TheVault.Models
         public DbSet<Item> Items { get; set; }
         public DbSet<Category> Categories { get;  set;}
         public DbSet<Producer> Producers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+       
     }
 }
